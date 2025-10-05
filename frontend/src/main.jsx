9a980f334d3b70327f08345dee6ab4bf3afeb470
Vivien <vivien.stampfer@gmail.com>
2025-10-05 18:53:50 +0200

    unregisterworker

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
-import { register } from './serviceWorkerRegistration';
+import { unregister } from './serviceWorkerRegistration';
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -19,4 +19,4 @@ createRoot(document.getElementById('root')).render(
   </StrictMode>,
 )
 // Registriraj service worker
-register();
+unregister();
